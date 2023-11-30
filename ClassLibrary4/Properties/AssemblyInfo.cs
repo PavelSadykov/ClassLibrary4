9742e774e6537fd796ecdd284e4b5304ebdbd00e
PavelSadykov <2439747@gmail.com>
2023-11-30 23:03:16 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ClassLibrary4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibrary4")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2d13a828-d8a7-4f81-8630-aa2950f11ceb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ClassLibrary4/TextManipulator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary4
+{
+    public class TextManipulator
+    {
+        public static bool IsPalindrome(string text) 
+        {
+            // Удаление знаков препинания и пробелов, приведение к нижнему регистру
+            string cleanText = new string(text.Where(char.IsLetter).ToArray()).ToLower();
+
+            // Проверка на палиндром
+            for (int i = 0; i < cleanText.Length / 2; i++)
+            {
+                if (cleanText[i] != cleanText[cleanText.Length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+
+        public static int CountSentences(string text)
+        {
+            // Подсчет количества предложений (предполагаем, что предложения разделены точкой)
+            string[] sentences = text.Split('.');
+            return sentences.Length - 1;
+        }
+
+        public static string ReverseString(string text)
+        {
+            // Переворот строки
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
